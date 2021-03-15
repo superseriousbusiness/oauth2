1f7842217e576150b1cd0eef4343f4b27814153c
tsmethurst <tobi.smethurst@klarrio.com>
2021-03-15 17:41:02 +0100

    update weird 'main' interface

--- store.go
+++ store.go
@@ -5,8 +5,9 @@ import "context"
 type (
 	// ClientStore the client information storage interface
 	ClientStore interface {
-		// according to the ID for the client information
 		GetByID(ctx context.Context, id string) (ClientInfo, error)
+		Set(ctx context.Context, id string, cli ClientInfo) error
+		Delete(ctx context.Context, id string) error
 	}
 
 	// TokenStore the token information storage interface
